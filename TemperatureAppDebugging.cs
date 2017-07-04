8ba5b0070e92b8f29a89e9f8fb074aeea6875ad5
PMacald <peter.i.macaldowie@gmail.com>
2017-07-04 15:43:56 +0100

    Add files via upload
    Made the program adhere more to OO standards.

--- TemperatureAppDebugging.cs
+++ TemperatureAppDebugging.cs
@@ -7,28 +7,30 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-public struct DataEntry
-{
-    public string dateAndTime { get; set; }
-    public string serialNo { get; set; }
-    public string reading { get; set; }
-    public string units { get; set; }
-    public void getValues(string d, string s, string r, string u)
-    {
-        //Eliminate double quotes for output
-        dateAndTime = Regex.Replace(d,"\"","");
-        serialNo = Regex.Replace(s, "\"", "");
-        reading = Regex.Replace(r, "\"", "");
-        units = Regex.Replace(u, "\"", "");
-    }
-}
 
-namespace ConsoleApplication1
+
+namespace TemperatureApp
+
 {
-    class Program
+    public class record
     {
-        static void Main(string[] args)
+        public string dateAndTime { get; set; }
+        public string serialNo { get; set; }
+        public string reading { get; set; }
+        public string units { get; set; }
+        public void getValues(string d, string s, string r, string u)
         {
+            //Eliminate double quotes for output
+            dateAndTime = Regex.Replace(d, "\"", "");
+            serialNo = Regex.Replace(s, "\"", "");
+            reading = Regex.Replace(r, "\"", "");
+            units = Regex.Replace(u, "\"", "");
+        }
+    }
+
+    public class ConsoleWindow
+    {
+        public static string checkFile() {
             string prompt = "Please enter the filepath of the data: ";
             bool fileFound = false;
             string pathname = "";
@@ -50,10 +52,16 @@ namespace ConsoleApplication1
                 }
 
             }
-            //string pathname = @"C:\Temperature_app\test.csv";
 
+            return pathname;
+            }
+
+        public static List<record> displayData(string pathname)
+        {
+            
             List<string> lines = new List<string>();
-            List<DataEntry> dataList = new List<DataEntry>();
+            
+            List<record> dataList = new List<record>();
             //int counter = 0;
             foreach (var row in File.ReadAllLines(pathname))
             {
@@ -63,16 +71,89 @@ namespace ConsoleApplication1
                 //rowArray contains data in each specified row, in array format
                 string[] rowArray = row.Split(',');
 
-                DataEntry rowStruct = new DataEntry();
+                record rowObj = new record();
 
                 //Call on method to fill row's data structure with record's column entries
-                rowStruct.getValues(rowArray[0], rowArray[1], rowArray[2], rowArray[3]);
-                Console.WriteLine("{0} \t {1} \t {2} \t {3}", rowStruct.dateAndTime, rowStruct.serialNo, rowStruct.reading, rowStruct.units);
+                rowObj.getValues(rowArray[0], rowArray[1], rowArray[2], rowArray[3]);
+                Console.WriteLine("{0} \t {1} \t {2} \t {3}", rowObj.dateAndTime, rowObj.serialNo, rowObj.reading, rowObj.units);
 
-                    dataList.Add(rowStruct);
+                dataList.Add(rowObj);
 
 
             }
+
+            return dataList;
+        }
+
+        
+        public static void consoleRequest()
+        {
+            bool finished = false;
+            string prompt = "";
+            while (!finished)
+            {
+                Console.WriteLine("What would you like to do? Type 'help' for more information");
+                string userResponse = Console.ReadLine();
+                switch (userResponse)
+                {
+                    case "help":
+                        {
+                            Console.Write(@"Commands you can use: 
+                            changetemp : Allows you to input a new temperature at which a warning will be produced
+                            
+                            ");
+                            break;
+                        }
+
+                    case "changetemp":
+                        {
+                            prompt = "Please enter the warning temperature you would like to set: ";
+                            bool satisfied = false;
+                            while (!satisfied)
+                            {
+                                Console.WriteLine(prompt);
+                                string temp = Console.ReadLine();
+                                if (float.Parse(temp) <= 0)
+                                {
+                                    Console.WriteLine("This is an invalid temperature. Please try again");
+                                }
+                                else
+                                {
+
+                                    StreamWriter file = new StreamWriter(@"C:\Temperature_app\WarningTemp.txt");
+                                    file.WriteLine(temp);
+                                    Console.WriteLine(temp);
+                                    file.Close();
+                                    satisfied = true;
+                                }
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Sorry, that command is invalid. Please try again. Note: Type 'help' for more information.");
+                            break;
+                        }
+
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathname = ConsoleWindow.checkFile();
+            //string pathname = @"C:\Temperature_app\test.csv";
+            List<record> dataList = new List<record>();
+            dataList = ConsoleWindow.displayData(pathname);
+            
 
             string warningTemp;
             //Get temperature at which a warning will be produced
@@ -86,8 +167,8 @@ namespace ConsoleApplication1
 
             Console.WriteLine(tempFloat.GetType());
             string timeOfError = "";
-                //Console.Write(dataList);
-                bool error = false;
+            //Console.Write(dataList);
+            bool error = false;
             foreach (var dataRow in dataList)
             {
                 try
@@ -99,62 +180,19 @@ namespace ConsoleApplication1
                         break;
                     }
                 }
-                catch(FormatException)
+                catch (FormatException)
                 {
-                    //following line is for later debugging
-                    //Console.WriteLine("This is the first line");
+                    continue;
                 }
-           }
+            }
             if (error)
             {
-                Console.WriteLine("A temperature of over {0} degrees has been recorded. It was recorded at {1}", warningTemp,timeOfError);
+                Console.WriteLine("A temperature of over {0} degrees has been recorded. It was recorded at {1}", warningTemp, timeOfError);
             }
-            Console.WriteLine("What would you like to do? Type 'help' for more information");
-            string userResponse = Console.ReadLine();
-            switch (userResponse)
-            {
-                case "help":
-                    {
-                        Console.Write(@"Commands you can use: 
-                            changetemp : Allows you to input a new temperature at which a warning will be produced
-                            
-                            ");
-                        break;
-                    }
 
-                case "changetemp":
-                    {
-                        prompt = "Please enter the warning temperature you would like to set: ";
-                        bool satisfied = false;
-                        while (!satisfied)
-                        {
-                            Console.WriteLine(prompt);
-                            string temp = Console.ReadLine();
-                            if (float.Parse(temp) < 0)
-                            {
-                                Console.WriteLine("This is an invalid temperature. Please try again");
-                            }
-                            else
-                            {
+            ConsoleWindow.consoleRequest();
 
-                                satisfied = true;
-                            }
-                        }
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine("Sorry, that command is invalid. Please try again. Note: Type 'help' for more information.");
-                        break;
-                    }
-                 
-            }
-            
-            
-             
             Console.ReadKey();
-       }
-   }
-
-
+        }
+    }
 }
